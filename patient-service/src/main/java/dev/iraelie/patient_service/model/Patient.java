687fe1208ca945a7bdd152697813374d69d45377
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-22 22:30:31 +0200

    Change type of Id to UUID and made it primary key in the table

--- patient-service/src/main/java/dev/iraelie/patient_service/model/Patient.java
+++ patient-service/src/main/java/dev/iraelie/patient_service/model/Patient.java
@@ -1,9 +1,16 @@
 package dev.iraelie.patient_service.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
 
 @Entity
 public class Patient {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
     private String name;
 }
