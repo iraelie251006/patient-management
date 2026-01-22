05260dc23cb5332ee0158f54877d2716e97737b1
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-22 22:37:32 +0200

    Added name, email, address, DOB, registeredData field to the patient model

--- patient-service/src/main/java/dev/iraelie/patient_service/model/Patient.java
+++ patient-service/src/main/java/dev/iraelie/patient_service/model/Patient.java
@@ -1,10 +1,10 @@
 package dev.iraelie.patient_service.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
@@ -12,5 +12,21 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @NotNull
     private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
 }
